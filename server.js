da80861c5445f3b0cb66ff7ac8fe74f76c3e684a
Veeasha <veeasha@gmail.com>
2026-03-25 01:33:54 +0200

    server2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-app.use(express.static(path.resolve(process.cwd(), "public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 let waitingPlayer = null;
 
