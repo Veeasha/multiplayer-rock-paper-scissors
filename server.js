da1cbc9298e965af9059d1dace12706ff26beec0
Veeasha <veeasha@gmail.com>
2026-03-25 01:42:23 +0200

    server5

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ function send(ws, data) {
     ws.send(JSON.stringify(data));
   }
 }
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 function getWinner(choice1, choice2) {
   if (choice1 === choice2) return "draw";
 
